9003e66c2ca5635ba1645a96c67b63cdbd888a9f
reys66223-art <reys66223@gmail.com>
2025-12-17 00:38:16 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// File ini siap untuk menambahkan interaktivitas pada halaman.
+// Contoh: console.log untuk memastikan file terhubung dengan baik.
+console.log("Portal Ujian Online MTS Islamyah Bindu - Siap!");
+
+// Anda bisa menambahkan kode JavaScript di sini, misalnya:
+// - Validasi form
+// - Animasi saat scroll
+// - Interaksi dengan tombol
